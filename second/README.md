7ebeba447434d61dd1753dc457ed5b34305e4038
Sergio Soto <scots4ever@gmail.com>
2015-05-10 23:40:02 +0200

    Second problem solved. Alpha version without lambda, only iterate with index over one of them

---
+++ second/README.md
@@ -0,0 +1,3 @@
+# Problem 2
+
+Write a function that combines two lists by alternatingly taking elements. For example: given the two lists [a, b, c] and [1, 2, 3], the function should return [a, 1, b, 2, c, 3].
---
+++ second/solution.py
@@ -0,0 +1,20 @@
+#!/bin/python
+# -*- coding: utf-8 -*-
+
+# starts at 23:23 sun 10 may
+# end at xxx sun 10 may
+
+def get_sol(array1, array2):
+    sol = []
+    for index in range(0, len(array1) - 1):
+        sol.append(array1[index])
+        sol.append(array2[index])
+    return sol
+
+
+if __name__ == "__main__":
+    array1 = [0,1,2,3,4,5]
+    array2 = ['a', 'b', 'c', 'd', 'e', 'f']
+    print "Inputs: \n\tarray1 => ", array1, "\n\tarray2 => ", array2
+    sol = get_sol(array1, array2)
+    print sol
